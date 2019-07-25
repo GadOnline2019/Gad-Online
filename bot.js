7f9a74100e2f50fc0bf594e9ca7701f856153dc1
GadOnline2019 <53317430+GadOnline2019@users.noreply.github.com>
2019-07-25 23:09:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["581212333372735606"]
+const developers = ["448904550032736256"]
 const adminprefix = "!";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
